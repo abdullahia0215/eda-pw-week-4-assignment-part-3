092b98f46fd45be67b0af32417d428fabad32c69
Abdi <abdullahia0215@gmail.com>
2024-04-12 11:45:14 -0500

    completed 1

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,18 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+console.log(basket);
+
+function addItem(item){
+    basket.push(item);
+    if (basket.length > 0){
+        return true;
+    }
+}
+
+console.log(addItem('kale'));
 
 
 
